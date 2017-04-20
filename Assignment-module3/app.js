3247c89528e95f67919b2ef875961a318e53974b
jiwanpokharel89 <jpokharel@luc.edu>
2017-04-20 00:05:46 -0500

    Added the template that works for Assignment-3

---
+++ Assignment-module3/app.js
@@ -0,0 +1,87 @@
+(function(){
+
+'use strict';
+
+angular.module('NarrowItDownApp',[])
+       .controller('NarrowItDownController',NarrowItDownController)
+       .service('MenuSearchService',MenuSearchService)
+       .directive('foundItems',FoundItems);
+
+function FoundItems(){
+  var ddo={
+    templateUrl: 'foundItems.html'
+    // scope:{
+    //    narrowList: '=foundItems'
+    //  }
+      // title: '@',
+      // onRemove: '<'
+    // },
+    // controller: FoundItemsDirectiveController,
+    // controllerAs: 'narrowList',
+    // bindToController: true
+  };
+  return ddo;
+}
+
+// function FoundItemsDirectiveController(){
+//   var list=this;
+//
+//   list.menuItemsInList=function(){
+//     if(list.foundItems.length>0)
+//       return true;
+//     return false;
+//   }
+// }
+
+NarrowItDownController.$inject=['MenuSearchService','$scope'];
+function NarrowItDownController(MenuSearchService,$scope){
+    var menu = this;
+
+    menu.searchTerm = "";
+    menu.found = [];
+    menu.emptyMessage="";
+
+    menu.removeItem = function(index){
+      menu.found.splice(index,1);
+    }
+
+    menu.narrowItDown = function () {
+      if ( menu.searchTerm.trim() ) {
+        menu.found = MenuSearchService.getMatchedMenuItems(menu.searchTerm);
+       if (menu.found.length > 0){
+        menu.emptyMessage="";
+      }
+
+      } else {
+        menu.found = [];
+        menu.emptyMessage='Nothing Found';
+      }
+
+    };
+
+  }
+
+MenuSearchService.$inject=['$http'];
+function MenuSearchService($http){
+  var service=this;
+
+  service.getMatchedMenuItems=function(searchTerm){
+
+    var items = [];
+    searchTerm = searchTerm.trim().toLowerCase();
+    $http({
+            method: "GET",
+            url: ("https://davids-restaurant.herokuapp.com/menu_items.json")
+        }).then(function (response) {
+              for ( var i = 0 ; i < response.data.menu_items.length ; i++ ) {
+                  if ( ( response.data.menu_items[i].description.toLowerCase().indexOf(searchTerm)) !== -1 ) {
+                    items.push( response.data.menu_items[i] );
+                  }
+              }
+        }).catch(function (error) {
+                console.log("Error while retrieving the data.");
+        });
+    return items;
+  };
+}
+})();
